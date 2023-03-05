7f0c347e16c5d492ff36b89af4f107bbce4168f1
wumbles <77227394+wumbles@users.noreply.github.com>
2023-03-04 21:53:31 -0700

    UPDATE: changed font size

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -32,7 +32,7 @@ export const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to My Portfolio!</span>
+            <span className="tagline">Welcome to My Portfolio</span>
             <h1>
               {"Lucas Martinsen, "}
               <span className="wrap">
